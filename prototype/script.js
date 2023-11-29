a2937479fc1b383dd580fadb1c2cc2fbc75ff22a
Rikdas123 <142583158+Rikdas123@users.noreply.github.com>
2023-11-29 13:24:40 +0530

    Add files via upload

---
+++ prototype/script.js
@@ -0,0 +1,9 @@
+let search = document.querySelector('.search');
+document.querySelector('#search').onclick =() => {
+    search.classList.toggle('active');
+}
+
+let favorite = document.querySelector('.favorite');
+document.querySelector('#favorite').onclick =() => {
+    search.classList.toggle('active');
+}
